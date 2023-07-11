7e07eabbc11a060376d32b1df53caf9a5d9c5156
Luis Francisco Contreras Gonzalez <luisfranciscocontrerasgonzalez@MacBook-Air-de-Luis-2.local>
2023-07-11 18:38:16 -0500

    add more

--- List_Problems/Merge_2_SortedLists.py
+++ List_Problems/Merge_2_SortedLists.py
@@ -16,3 +16,35 @@ list1 = [1, 3, 5, 7]
 list2 = [2, 4, 6, 8]
 merged_list = merge_sorted_lists(list1, list2)
 print(merged_list)  # Output: [1, 2, 3, 4, 5, 6, 7, 8]
+
+# Implementing a custom merging algorithm:
+"""
+we iterate through both lists using two pointers (i and j). 
+At each iteration, we compare the current elements from both lists and append the smaller one to the merged_list. 
+We increment the corresponding pointer and continue until we reach the end of either list. 
+Finally, we append any remaining elements from the lists to the merged_list.
+"""
+
+def merge_sorted_lists(list1, list2):
+    merged_list = []
+    i = j = 0
+
+    while i < len(list1) and j < len(list2):
+        if list1[i] <= list2[j]:
+            merged_list.append(list1[i])
+            i += 1
+        else:
+            merged_list.append(list2[j])
+            j += 1
+
+    # Append any remaining elements from the lists
+    merged_list.extend(list1[i:])
+    merged_list.extend(list2[j:])
+
+    return merged_list
+
+# Example usage
+list1 = [1, 3, 5, 7]
+list2 = [2, 4, 6, 8]
+merged_list = merge_sorted_lists(list1, list2)
+print(merged_list)  # Output: [1, 2, 3, 4, 5, 6, 7, 8]
