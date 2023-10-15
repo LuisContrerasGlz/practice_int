60b39975ae94b53a855445dae7371b734ddc8606
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-15 04:04:07 -0600

    add problem

--- Real_Int/check_substring.py
+++ Real_Int/check_substring.py
@@ -22,3 +22,35 @@ Notice that the answer must be a substring, "pwke" is a subsequence and not a su
 
 
 """
+
+def find_longest_unique_substring(input_string):
+    
+    # Variables para rastrear la subcadena actual y la subcadena más larga
+
+    current_substring = ""  # Subcadena actual que se está construyendo
+    longest_substring = ""  # Subcadena más larga encontrada hasta el momento
+
+    # Iteramos a través de la cadena de entrada
+    for i in range(len(input_string)):
+        # Comprobamos si el carácter actual ya está en la subcadena actual
+        if (input_string[i] in current_substring):
+            # Si el carácter está duplicado, comparamos longitudes y actualizamos si es necesario
+            if (len(current_substring) > len(longest_substring)):
+                longest_substring = current_substring
+            # Restablecemos la subcadena actual para comenzar una nueva
+            current_substring = input_string[i]
+        else:
+            # Si el carácter es único, lo agregamos a la subcadena actual
+            current_substring = current_substring + input_string[i]
+    
+    # Comprobamos una vez más al final para asegurarnos de que la última subcadena sea la más larga si es el caso
+    if (len(current_substring) > len(longest_substring)):
+        longest_substring = current_substring
+
+    # Devolvemos la subcadena más larga con caracteres únicos encontrada
+    return longest_substring
+
+# Ejemplo de uso
+input_string = "abcaabcde"
+result = find_longest_unique_substring(input_string)
+print(result)  # Debe imprimir "abcde"
