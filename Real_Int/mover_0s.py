6da8b58b36e87d496dd7de54af2740088e3636bb
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-16 00:35:14 -0600

    add problem

--- Real_Int/mover_0s.py
+++ Real_Int/mover_0s.py
@@ -5,39 +5,39 @@ def cero_to_the_right(array):
     ceroTR = []
 
     # Agrega los elementos diferentes de cero a la nueva lista
-    for i in range(len(array)):
-        if array[i] != 0:
-            ceroTR.append(array[i])
+    for element in array:
+        if element != 0:
+            ceroTR.append(element)
 
     # Calcula la cantidad de ceros ignorados
     num_zeros_ignored = len(array) - len(ceroTR)
 
     # Agrega los ceros faltantes al final de la nueva lista
-    for i in range(num_zeros_ignored):
+    for _ in range(num_zeros_ignored):
         ceroTR.append(0)
 
     # Retorna la lista modificada
     return ceroTR
-
 
 def cero_to_the_left(array):
     # Crea una nueva lista para almacenar los elementos reorganizados
     ceroTL = []
 
     # Crea una lista sin los ceros, manteniendo el orden original
-    for i in range(len(array)):
-        if array[i] != 0:
-            ceroTL.append(array[i])
+    for element in array:
+        if element != 0:
+            ceroTL.append(element)
 
     # Calcula la cantidad de ceros ignorados
     num_zeros_ignored = len(array) - len(ceroTL)
 
     # Agrega los ceros al principio de la nueva lista
-    for i in range(num_zeros_ignored):
+    for _ in range(num_zeros_ignored):
         ceroTL.insert(0, 0)
 
     # Retorna la lista modificada
     return ceroTL
+
 
 
 array = [0,1,5,4,32,80,0,4,0,1]
