7ea22eae37c29cb1cc9d952540de4cf1eb79c895
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-17 12:15:41 -0600

    add problem

---
+++ Real_Int/swap_values_without_3rd_variable.py
@@ -0,0 +1,10 @@
+# Given 2 numbers, swap them without the 3rd variable.
+
+x = 5
+y = 10
+
+x, y = y, x
+
+print(x, y)
+
+# In Python, you can swap two numbers without using a temporary variable by using arithmetic operations and tuple unpacking.
