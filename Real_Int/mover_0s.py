53f6865fc950aac9fbabc76a18bf00e38c6cbd93
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-16 23:57:43 -0600

    refarctor, case in names

--- Real_Int/mover_0s.py
+++ Real_Int/mover_0s.py
@@ -44,3 +44,21 @@ array = [0,1,5,4,32,80,0,4,0,1]
 print(array)
 print(cero_to_the_right(array))
 print(cero_to_the_left(array))
+
+
+"""
+
+The time complexity of the provided code is O(n), where 'n' is the length of the input array array.
+
+Here's the breakdown:
+
+The first for loop iterates through the elements in the input array array, which takes O(n) time.
+
+The second for loop iterates a number of times equal to num_zeros_ignored. The value of num_zeros_ignored is determined by subtracting the length of ceroTR from the length of the original array, which is still O(n).
+
+All other operations in the code run in constant time (O(1)).
+
+Therefore, the overall time complexity is O(n) because the dominant factor in the code's execution time is the iteration over the elements in the input array.
+
+
+"""
