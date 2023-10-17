2ce6f16092ac1759b7c2f97bb523a1629f9d3e6e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-16 23:55:42 -0600

    refarctor, case in names

--- Real_Int/aguja_pajar.py
+++ Real_Int/aguja_pajar.py
@@ -18,3 +18,15 @@ def aguja(palabra,arreglo):
 arreglo=['hola','adios','bye','hello','aguja','otro','some']
 palabra='aguja'
 print(aguja(palabra,arreglo))
+
+"""
+
+The time complexity (big O notation) of the given code is O(n), where 'n' is the length of the input array arreglo.
+
+The primary operation affecting the time complexity is the arreglo.index(palabra) function, 
+which searches for the index of the palabra in the array. 
+
+In the worst case, this function may have to scan through the entire array to find the index of the given word. 
+Therefore, the time complexity is linear with respect to the size of the array.
+
+"""
