2499dfbbf58eca52b5c47748c6e59f37babb006d
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-16 23:59:46 -0600

    refarctor, case in names

--- Real_Int/eliminar_repetidos_imprimir.py
+++ Real_Int/eliminar_repetidos_imprimir.py
@@ -12,3 +12,20 @@ def elimina_repetidos(arreglo):
 arreglo = ["Hola", "Mundo", "Hola", "Python", "Mundo", "Programación"]
 
 print(elimina_repetidos(arreglo))
+
+
+"""
+
+The time complexity of the provided code is O(n^2), where 'n' is the length of the input array arreglo.
+
+Here's the breakdown:
+
+The code uses a for loop to iterate over each element in the input array arreglo. This loop contributes O(n) time complexity.
+
+Inside the loop, it uses the if palabra not in palabras_unicas condition to check whether the current element is already in the palabras_unicas list. Checking whether an element is in a list has a time complexity of O(n) for each element. Since this check is performed within the loop, the worst-case time complexity is O(n) for the check.
+
+If the current element is not in palabras_unicas, it appends the element to the palabras_unicas list. The append operation typically runs in constant time, O(1), but since it's executed within the loop, it contributes O(n) to the overall time complexity.
+
+As a result, the total time complexity is O(n) for the outer loop multiplied by O(n) for the inner loop, which results in O(n^2). This quadratic time complexity makes the code less efficient, especially for large input arrays. 
+
+"""
