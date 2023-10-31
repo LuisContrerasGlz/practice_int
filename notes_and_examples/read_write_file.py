634cd76318792f22929e3fa783ee58c0b5bd2d0e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-10-31 00:45:23 -0600

    added example

--- notes_and_examples/read_write_file.py
+++ notes_and_examples/read_write_file.py
@@ -19,3 +19,22 @@ Subsequently, the file is opened again with the mode 'r' (read mode) to read its
 The read() method reads the content and then it's printed to the console.
 
 """
+
+# Writing to a file (appending)
+with open('example.txt', 'a') as file:  # Use 'a' mode for append
+    file.write("\nThis content will be appended to the file.")
+
+# Reading from the file
+with open('example.txt', 'r') as file:
+    content = file.read()
+    print("Content of the file:")
+    print(content)
+
+
+"""
+
+The open() function uses 'a' mode for append, which will allow the content to be added to the end of the file without overwriting existing content.
+
+A newline character (\n) is used to ensure the new content starts on a new line.
+
+"""
