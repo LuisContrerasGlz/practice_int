a7f0b27e3dfacd227801944052a52543777185dd
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 00:20:03 -0600

    add explain

--- new_practice_problems/duplicate_characters_string.py
+++ new_practice_problems/duplicate_characters_string.py
@@ -1,1 +1,31 @@
 # Program to find the duplicate characters in String.
+
+"""
+
+Approach:
+
+Iterate Through String:
+
+Traverse each character in the given string.
+Counting Occurrences:
+
+For each character, maintain a count of its occurrences.
+Identify Duplicates:
+
+If the count of a character is more than 1, mark it as a duplicate.
+Output:
+
+Display or store the duplicate characters.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Identify duplicate characters:
+
+Duplicates: 'g', 'r', 'm'
+Note:
+
+Decide whether the comparison is case-sensitive or not.
+Ensure that duplicates are identified correctly, considering their frequency.
+
+"""
