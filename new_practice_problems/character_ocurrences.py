744d1b34d6cee1aaaf50d596868b9f97ebce4d1c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 00:17:25 -0600

    add explain

--- new_practice_problems/character_ocurrences.py
+++ new_practice_problems/character_ocurrences.py
@@ -1,1 +1,33 @@
 # Program to count the occurrences of a particular character in a String
+
+"""
+
+Approach:
+
+Iterate Through the String:
+
+Traverse each character in the given string.
+Counting:
+
+Maintain a counter to keep track of the number of occurrences of the specified character.
+Comparison:
+
+Check each character against the specified character.
+If the character matches, increment the counter.
+Example:
+Let's take an example:
+
+String: "programming"
+Character to count: 'g'
+Count the occurrences:
+
+String: "programming"
+Character to count: 'g'
+Occurrences of 'g': 2
+
+Note:
+
+Ensure that the comparison is case-sensitive unless specified otherwise.
+Handle the situation where the specified character may not be present in the string.
+
+"""
