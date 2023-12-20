bbf124ffd49d13d0c5c1cebcb20443722fd4bbf4
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 00:18:02 -0600

    add explain

--- new_practice_problems/common_elements_2array.py
+++ new_practice_problems/common_elements_2array.py
@@ -1,1 +1,32 @@
 # Program to Find Common Elements Between Two Arrays.
+
+"""
+
+Approach:
+
+Iterate Through Arrays:
+
+Traverse each element in the first array.
+Comparison:
+
+For each element in the first array, compare it with each element in the second array.
+Identify Common Elements:
+
+If an element is found in both arrays, mark it as a common element.
+Output:
+
+Display or store the common elements.
+Example:
+Let's take an example:
+
+Array 1: [3, 5, 7, 9, 11]
+Array 2: [5, 9, 12, 15]
+Identify common elements:
+
+Common elements: [5, 9]
+Note:
+
+The order of common elements may or may not be preserved, depending on the specific requirements.
+Ensure that duplicates are handled appropriately.
+
+"""
