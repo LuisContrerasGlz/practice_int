08f8f59b74eb84da01164953277b26ebd228f4ce
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 00:19:18 -0600

    add explain

--- new_practice_problems/count_vowels.py
+++ new_practice_problems/count_vowels.py
@@ -1,1 +1,31 @@
 # Count the number of vowels in the given String
+
+
+"""
+
+Approach:
+
+Iterate Through the String:
+
+Traverse each character in the given string.
+Check for Vowels:
+
+For each character, check if it is a vowel (either uppercase or lowercase).
+Counting:
+
+Maintain a counter to keep track of the number of vowels encountered.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Count the number of vowels:
+
+Input String: "programming"
+Number of vowels: 3 (o, o, i)
+
+Note:
+
+Decide whether the comparison is case-sensitive or not.
+Include both uppercase and lowercase vowels in the check.
+
+"""
