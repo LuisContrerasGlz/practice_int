aa90a1fa8b72961195c4b8d91974ae392914cecf
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 00:20:39 -0600

    add explain

--- new_practice_problems/duplicate_words_array.py
+++ new_practice_problems/duplicate_words_array.py
@@ -1,1 +1,31 @@
 # Program to Find Duplicate Words in a Given String
+
+"""
+
+Approach:
+
+Tokenization:
+
+Split the input string into individual words. This may involve using spaces or other delimiters.
+Counting Occurrences:
+
+For each word, maintain a count of its occurrences.
+Identify Duplicates:
+
+If the count of a word is more than 1, mark it as a duplicate.
+Output:
+
+Display or store the duplicate words.
+Example:
+Let's take an example:
+
+Input String: "This is a programming is a language programming."
+Identify duplicate words:
+
+Duplicates: 'is', 'a', 'programming'
+Note:
+
+Decide whether the comparison is case-sensitive or not.
+Handle punctuation marks and other special characters appropriately.
+
+"""
