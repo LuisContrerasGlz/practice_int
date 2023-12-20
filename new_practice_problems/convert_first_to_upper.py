52f8e09d200b5739c219bf3250865b0d5215a595
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 00:18:41 -0600

    add explain

--- new_practice_problems/convert_first_to_upper.py
+++ new_practice_problems/convert_first_to_upper.py
@@ -1,1 +1,28 @@
 # Program to convert the first letter of each word in a string to uppercase.
+
+"""
+
+Approach:
+
+Split the String:
+
+Break down the input string into individual words. This involves separating the string based on spaces or other delimiters.
+Uppercase Transformation:
+
+For each word obtained, transform the first letter to uppercase.
+Reassemble the String:
+
+Combine the modified words to form the final string.
+Example:
+Let's take an example:
+
+Input String: "hello world"
+Convert the first letter of each word to uppercase:
+
+Output String: "Hello World"
+Note:
+
+Handle cases where the input string might have different word separators.
+Ensure that the transformation is applied only to the first letter of each word.
+
+"""
