5cf3c0fba390da9146dea1c230a6860135313e0c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-20 18:12:04 -0600

    add description

--- new_practice_problems/element_present.py
+++ new_practice_problems/element_present.py
@@ -1,1 +1,30 @@
 # Program to check if an element present in the array or not.
+
+"""
+
+Approach:
+
+Iterate Through the Array:
+
+Traverse each element in the array.
+Comparison:
+
+For each element, compare it with the target element to check for a match.
+Result:
+
+If a match is found, the element is present in the array.
+If no match is found after checking all elements, the element is not present.
+Example:
+Let's take an example:
+
+Array: [3, 5, 7, 9, 11]
+Element to check: 7
+Check if 7 is present in the array:
+
+Result: Yes, 7 is present in the array.
+Note:
+
+Decide whether the comparison is case-sensitive or not.
+Handle situations where the element may appear more than once in the array.
+
+"""
