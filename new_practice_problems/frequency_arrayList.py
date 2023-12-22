f457191b9533d8ecde6b143799d303b28b71dc45
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:05:34 -0600

    add description

--- new_practice_problems/frequency_arrayList.py
+++ new_practice_problems/frequency_arrayList.py
@@ -1,1 +1,33 @@
 # Program to find the frequency of elements in an ArrayList
+
+
+"""
+
+Approach:
+
+Initialize Frequency Count:
+
+Create a data structure (such as a HashMap or another ArrayList) to store the frequency count of each unique element.
+Iterate Through ArrayList:
+
+Traverse each element in the ArrayList.
+Count Frequency:
+
+For each element, update its frequency count in the data structure.
+Result:
+
+The data structure now contains the frequency of each unique element in the ArrayList.
+Example:
+Let's take an example:
+
+ArrayList: [2, 3, 2, 5, 6, 3, 4, 7, 6]
+Count the frequency of each element:
+
+Frequency Count: {2: 2, 3: 2, 5: 1, 6: 2, 4: 1, 7: 1}
+Note:
+
+Ensure that the data structure used for counting is suitable for the task.
+Handle situations where the ArrayList may be empty.
+
+
+"""
