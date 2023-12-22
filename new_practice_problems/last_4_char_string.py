e284afb2387213e5ec661e535c0a83b22be14f47
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:06:04 -0600

    add description

--- new_practice_problems/last_4_char_string.py
+++ new_practice_problems/last_4_char_string.py
@@ -1,1 +1,29 @@
 # Extract Last 4 characters of any String
+
+"""
+
+Approach:
+
+Check String Length:
+
+Verify that the length of the string is at least 4.
+Substring Extraction:
+
+Use substring operations to extract the last 4 characters of the string.
+Result:
+
+Return the extracted substring.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Extract the last 4 characters:
+
+Result: "ming"
+Note:
+
+If the string is less than 4 characters, handle it appropriately (e.g., return the whole string or a special indicator).
+The approach is generic and works for strings of any length.
+
+
+"""
