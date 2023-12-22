fb45b307f9923eb75cd6a7901081459fa350cbee
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:02:39 -0600

    add description

--- new_practice_problems/ocurrence_count_each_char.py
+++ new_practice_problems/ocurrence_count_each_char.py
@@ -1,1 +1,32 @@
 # Program to Count the Occurrences of Each Character
+
+"""
+
+Approach:
+
+Initialize Character Count:
+
+Create a data structure (such as a dictionary or an array) to store the frequency count of each character.
+Iterate Through String:
+
+Traverse each character in the given string.
+Count Occurrences:
+
+For each character, update its frequency count in the data structure.
+Result:
+
+The data structure now contains the count of occurrences for each character.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Count the occurrences of each character:
+
+Character Count: {'p': 1, 'r': 2, 'o': 1, 'g': 2, 'a': 1, 'm': 2, 'i': 1, 'n': 1}
+Note:
+
+Decide whether the comparison is case-sensitive or not.
+Punctuation marks and spaces may or may not be considered, depending on the requirements.
+
+
+"""
