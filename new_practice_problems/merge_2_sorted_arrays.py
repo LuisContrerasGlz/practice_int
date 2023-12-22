c0ce5bf36298f22b24386351428c54a48f3b9e44
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:07:10 -0600

    add description

--- new_practice_problems/merge_2_sorted_arrays.py
+++ new_practice_problems/merge_2_sorted_arrays.py
@@ -1,1 +1,34 @@
 # Program to merge two sorted arrays
+
+"""
+
+Approach:
+
+Initialize Pointers:
+
+Use two pointers to keep track of the current position in each of the two sorted arrays.
+Compare and Merge:
+
+Compare the elements at the current positions in both arrays.
+Add the smaller element to the merged array and move the pointer in that array.
+Handle Remaining Elements:
+
+Continue comparing and merging until one of the arrays is exhausted.
+If any elements remain in the non-empty array, add them to the merged array.
+Result:
+
+The merged array is the result, and it is also sorted.
+Example:
+Let's take an example:
+
+Array 1: [1, 3, 5, 7]
+Array 2: [2, 4, 6, 8]
+Merge the two arrays:
+
+Merged Array: [1, 2, 3, 4, 5, 6, 7, 8]
+Note:
+
+This approach takes advantage of the fact that the input arrays are already sorted.
+The time complexity of this solution is O(n), where n is the total number of elements in the merged array.
+
+"""
