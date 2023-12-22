6920fdc40798a34846b28e930af5a36133d1a2e7
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:02:55 -0600

    add description

--- new_practice_problems/first_letter_string.py
+++ new_practice_problems/first_letter_string.py
@@ -1,1 +1,27 @@
 # Print first letter of each word present in string
+
+"""
+
+
+Approach:
+
+Tokenization:
+
+Split the input string into individual words. This involves separating the string based on spaces or other delimiters.
+Print First Letter:
+
+For each word obtained, print the first letter.
+Example:
+Let's take an example:
+
+Input String: "hello world"
+Print the first letter of each word:
+
+Output: "h w"
+Note:
+
+Ensure that the output format is as desired, considering spacing or other requirements.
+Handle situations where words may be separated by different delimiters.
+
+
+"""
