344794976e4b8269161663efde4196bc7b8f1e99
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:01:52 -0600

    add description

--- new_practice_problems/factorial.py
+++ new_practice_problems/factorial.py
@@ -1,1 +1,26 @@
 # Program to find the factorial of the number.
+
+"""
+
+Definition:
+The factorial of a non-negative integer n is the product of all positive integers less than or equal to n. It is denoted by n!.
+
+
+
+Input:
+
+Receive a non-negative integer input for which the factorial needs to be calculated.
+Iteration:
+
+Use a loop to multiply the numbers from 1 to the given number.
+
+Result:
+
+The result of the multiplication is the factorial of the given number.
+
+Note:
+
+The factorial of 0 is defined to be 1.
+Factorial is not defined for negative numbers.
+
+"""
