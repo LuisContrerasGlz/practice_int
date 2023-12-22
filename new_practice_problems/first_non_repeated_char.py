bf2c87fe0f49051e2f8698d24aec9a5efc0d58d1
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:04:08 -0600

    add description

--- new_practice_problems/first_non_repeated_char.py
+++ new_practice_problems/first_non_repeated_char.py
@@ -1,1 +1,31 @@
 # Find first non repeated character in a string.
+
+
+"""
+
+Approach:
+
+Character Count:
+
+Count the occurrences of each character in the string.
+Find First Non-Repeated:
+
+Iterate through the string and check the count of each character.
+The first character with a count of 1 is the first non-repeated character.
+Result:
+
+Return the first non-repeated character found.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Find the first non-repeated character:
+
+Result: 'o'
+Note:
+
+If no non-repeated character is found, return an appropriate indicator (e.g., 'None' or a special character).
+Decide whether the comparison is case-sensitive or not.
+
+
+"""
