6eebb4f23eba46fc9eefaa889260256e8686c613
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:04:39 -0600

    add description

--- new_practice_problems/frequency_array_elements.py
+++ new_practice_problems/frequency_array_elements.py
@@ -1,1 +1,32 @@
 # Count the frequency of elements in an array.
+
+
+"""
+
+Approach:
+
+Initialize Frequency Count:
+
+Create a data structure (such as a dictionary or an array) to store the frequency count of each unique element.
+Iterate Through Array:
+
+Traverse each element in the array.
+Count Frequency:
+
+For each element, update its frequency count in the data structure.
+Result:
+
+The data structure now contains the frequency of each unique element in the array.
+Example:
+Let's take an example:
+
+Array: [2, 3, 2, 5, 6, 3, 4, 7, 6]
+Count the frequency of each element:
+
+Frequency Count: {2: 2, 3: 2, 5: 1, 6: 2, 4: 1, 7: 1}
+Note:
+
+Ensure that the data structure used for counting is suitable for the task.
+Handle situations where the array may be empty.
+
+"""
