eea9d832c38b1c4f1ea98559cdaa9469fbc152a7
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:15:33 -0600

    add description

--- new_practice_problems/swap_first_last_letters.py
+++ new_practice_problems/swap_first_last_letters.py
@@ -1,1 +1,30 @@
 # Program to swap the first and last letter in a string.
+
+"""
+
+Approach:
+
+Check String Length:
+
+Verify that the length of the string is at least 2 (to ensure there are both a first and last letter to swap).
+Swap Letters:
+
+Swap the characters at the first and last positions in the string.
+Result:
+
+The modified string after swapping is the result.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Swap the first and last letter:
+
+Result: "grogramminp"
+
+Note:
+
+Handle situations where the string is empty or has only one character.
+The solution works for strings of any length greater than or equal to 2.
+
+
+"""
