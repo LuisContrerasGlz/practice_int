4aa4e475774338655ee63522b8fb4aaba02a727f
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:12:49 -0600

    add description

--- new_practice_problems/remove_vowels_string.py
+++ new_practice_problems/remove_vowels_string.py
@@ -1,1 +1,32 @@
 # Program to remove vowels from a String
+
+"""
+
+Approach:
+
+Initialize Result String:
+
+Create an empty string to store the result without vowels.
+Iterate Through String:
+
+Traverse each character in the given string.
+Check for Vowels:
+
+For each character, check if it is a vowel.
+If it's not a vowel, add it to the result string.
+Result:
+
+The result string now contains the input string with vowels removed.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Remove vowels:
+
+Result: "prgrmmng"
+Note:
+
+Decide whether the comparison is case-sensitive or not.
+Consider whether you want to remove both uppercase and lowercase vowels.
+
+"""
--- new_practice_problems/segregate_0s_1s.py
+++ new_practice_problems/segregate_0s_1s.py
@@ -1,1 +1,32 @@
 # Program to segregate 0s and 1s in given array. Input = {1,0,1,1,0,1,1,0,0} Output = {0,0,0,0,1,1,1,1,1}
+
+"""
+
+Approach:
+
+Initialize Pointers:
+
+Use two pointers, one for the left (start) and one for the right (end) of the array.
+Traverse and Swap:
+
+Move the left pointer from the start until a 1 is encountered.
+Move the right pointer from the end until a 0 is encountered.
+Swap the elements at the left and right pointers.
+Result:
+
+Continue this process until the left pointer is greater than or equal to the right pointer.
+The array is now segregated with all 0s before 1s.
+Example:
+Let's take an example:
+
+Input Array: [1, 0, 1, 1, 0, 1, 1, 0, 0]
+Segregate 0s and 1s:
+
+Resulting Array: [0, 0, 0, 0, 1, 1, 1, 1, 1]
+
+Note:
+
+This is known as the Dutch National Flag problem.
+
+
+"""
