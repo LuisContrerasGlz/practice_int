d09f223d52a05e83eebc48f478e33f2d9f880d1b
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:14:46 -0600

    add description

--- new_practice_problems/string_len.py
+++ new_practice_problems/string_len.py
@@ -1,1 +1,29 @@
 # Program to find the length of String without using length() method
+
+"""
+
+Approach:
+
+Iterate Through Characters:
+
+Use a loop to traverse each character in the string.
+Count Characters:
+
+Maintain a counter to count the number of characters encountered during the iteration.
+Result:
+
+The counter value is the length of the string.
+Example:
+Let's take an example:
+
+Input String: "programming"
+Find the length of the string without using length() method:
+
+Result: 11
+Note:
+
+This approach provides an alternative way to determine the length of a string.
+Handle situations where the string may be empty.
+
+
+"""
