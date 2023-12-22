7ce1af00fadc55b25c5e85696327a3e02034beb0
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:17:11 -0600

    add description

--- new_practice_problems/unique_list_elements.py
+++ new_practice_problems/unique_list_elements.py
@@ -1,1 +1,36 @@
 # Program to check if all elements in a List are unique.
+
+
+"""
+
+Approach:
+
+Use a Data Structure:
+
+Choose a suitable data structure to keep track of unique elements encountered.
+Iterate Through List:
+
+Traverse each element in the list.
+Check for Uniqueness:
+
+For each element, check whether it has been encountered before.
+If it's a new element, add it to the data structure.
+If it's a duplicate, the list does not have all unique elements.
+Result:
+
+If the iteration completes without finding any duplicates, the list has all unique elements.
+Example:
+Let's take an example:
+
+Input List: [5, 3, 9, 1, 7, 2]
+Check if all elements are unique:
+
+Result: Yes, all elements are unique.
+
+Note:
+
+The choice of data structure may vary depending on the specific requirements.
+The time complexity of this solution is typically O(n), where n is the number of elements in the list.
+
+
+"""
