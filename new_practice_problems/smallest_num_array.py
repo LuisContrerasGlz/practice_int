eb16af9d36f4f32ba602d41dc2e6cda6273d8e24
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:13:29 -0600

    add description

--- new_practice_problems/smallest_num_array.py
+++ new_practice_problems/smallest_num_array.py
@@ -1,1 +1,34 @@
 # Program to find the smallest number in an array.
+
+"""
+
+Approach:
+
+Initialize Minimum:
+
+Assume the first element of the array as the minimum.
+Iterate Through Array:
+
+Traverse each element in the array.
+Update Minimum:
+
+For each element, compare it with the current minimum.
+If the element is smaller, update the minimum.
+Result:
+
+The minimum value found during the iteration is the result.
+Example:
+Let's take an example:
+
+Input Array: [5, 3, 9, 1, 7, 2]
+Find the smallest number:
+
+Result: 1
+
+Note:
+
+Handle situations where the array may be empty.
+If duplicates exist, the first occurrence of the smallest number is usually considered.
+
+
+"""
