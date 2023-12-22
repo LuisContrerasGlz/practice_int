74826631750276acac85e280d6de92cb5dbbb62b
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-21 20:08:28 -0600

    add description

--- new_practice_problems/number_of_words_string.py
+++ new_practice_problems/number_of_words_string.py
@@ -1,1 +1,30 @@
 # Count the number of words in given string.
+
+
+"""
+
+Approach:
+
+Tokenization:
+
+Split the input string into individual words. This involves separating the string based on spaces or other delimiters.
+Counting:
+
+Count the number of words obtained after tokenization.
+Result:
+
+The count of words is the result.
+Example:
+Let's take an example:
+
+Input String: "Hello, how are you doing?"
+Count the number of words:
+
+Result: 6 (Hello, how, are, you, doing)
+Note:
+
+Handle situations where there may be consecutive spaces or other delimiters.
+Punctuation marks may or may not be considered as part of a word, depending on the requirements.
+
+
+"""
