3d6852da7779a741b9e2e72845906ffb6332db36
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:14:10 -0600

    add description

--- new_practice_problems/sort_array.py
+++ new_practice_problems/sort_array.py
@@ -1,1 +1,37 @@
 # Program to sort an array
+
+"""
+
+Approach:
+
+Select a Sorting Algorithm:
+
+Choose a sorting algorithm based on the requirements and constraints.
+Implement the Chosen Algorithm:
+
+Implement the selected sorting algorithm in the code.
+Apply the Sorting Algorithm to the Array:
+
+Use the implemented algorithm to sort the elements of the array.
+Result:
+
+The array is now sorted in ascending order.
+Common Sorting Algorithms:
+
+Bubble Sort: Compare adjacent elements and swap them if they are in the wrong order. Repeat until no swaps are needed.
+Selection Sort: Select the smallest element and swap it with the first element. Repeat for the remaining unsorted portion.
+Insertion Sort: Build the sorted array one element at a time by repeatedly taking an element and inserting it into its correct position.
+Merge Sort: Divide the array into two halves, recursively sort each half, and then merge the sorted halves.
+Quick Sort: Choose a "pivot" element, partition the array into two sub-arrays based on the pivot, and recursively sort the sub-arrays.
+Example:
+Let's take an example using the Selection Sort algorithm:
+
+Input Array: [5, 3, 9, 1, 7, 2]
+Sort the array:
+
+Resulting Array: [1, 2, 3, 5, 7, 9]
+Note:
+
+The choice of sorting algorithm may depend on factors such as the size of the array, time complexity requirements, and stability.
+
+"""
