8deb8bb12167b1ebe046cadb106822754346497e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2023-12-22 17:16:33 -0600

    add description

--- new_practice_problems/third_largest_num.py
+++ new_practice_problems/third_largest_num.py
@@ -1,1 +1,35 @@
 # Program to find Third Largest Number in an Array
+
+"""
+
+Approach:
+
+Initialize Variables:
+
+Assume the first three elements of the array as the first, second, and third largest.
+Iterate Through Array:
+
+Traverse each element in the array.
+Update Largest Numbers:
+
+For each element, compare it with the current first, second, and third largest numbers.
+Update the variables accordingly.
+Result:
+
+The third largest number found during the iteration is the result.
+Example:
+Let's take an example:
+
+Input Array: [5, 3, 9, 1, 7, 2]
+Find the third largest number:
+
+Result: 3
+
+Note:
+
+Handle situations where the array may have fewer than three unique elements.
+If duplicates exist, the third largest unique number is usually considered.
+
+
+
+"""
