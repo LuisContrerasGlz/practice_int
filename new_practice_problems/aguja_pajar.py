f5b6520ce307a13b50ba8a7ca776ff39618c15a1
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-01-02 17:09:06 -0600

    add description

---
+++ new_practice_problems/aguja_pajar.py
@@ -0,0 +1,7 @@
+"""
+
+Encontrar la aguja en el pajar. 
+
+Es un arreglo de palabras revueltas y en alguna posicion está la palabra "aguja", tienes que encontrar la palabra e imprimir su posición.
+
+"""
