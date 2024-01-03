5f0ae323106af81085b33ab2049ed4e3024a0f79
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-01-03 16:27:18 -0600

    add description

--- Searching_Python/binary_search.py
+++ Searching_Python/binary_search.py
@@ -7,3 +7,9 @@ It is suitable for unsorted lists.
 Time complexity: O(n) in the worst case, where 'n' is the number of elements in the list.
 
 """
+
+def linear_search(arr, target):
+    for i, element in enumerate(arr):
+        if element == target:
+            return i
+    return -1
