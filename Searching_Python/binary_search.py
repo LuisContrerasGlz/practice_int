6f4027cda240344c4daeb27467a2c8bddc0a44ae
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-01-03 16:28:59 -0600

    add description

--- Searching_Python/binary_search.py
+++ Searching_Python/binary_search.py
@@ -7,3 +7,16 @@ It repeatedly divides the search interval in half until the target element is fo
 Time complexity: O(log n) in the worst case, where 'n' is the number of elements in the list.
 
 """
+
+def binary_search(arr, target):
+    low = 0, 
+    high= len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
