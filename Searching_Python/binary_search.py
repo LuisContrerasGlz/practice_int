38618643d55470516cfce6150fbb4621be4565ce
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-01-03 16:28:08 -0600

    add description

---
+++ Searching_Python/binary_search.py
@@ -0,0 +1,9 @@
+"""
+
+Binary Search:
+
+Binary search is a more efficient algorithm that works on sorted lists.
+It repeatedly divides the search interval in half until the target element is found.
+Time complexity: O(log n) in the worst case, where 'n' is the number of elements in the list.
+
+"""
