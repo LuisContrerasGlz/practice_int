fb946368a11c98ea201c5f263104ac3e9978954c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-01-03 16:24:39 -0600

    add description

---
+++ Searching_Python/binary_search.py
@@ -0,0 +1,9 @@
+"""
+
+Linear Search:
+
+Linear search is a simple and straightforward method that iterates through each element in a sequence until the desired element is found.
+It is suitable for unsorted lists.
+Time complexity: O(n) in the worst case, where 'n' is the number of elements in the list.
+
+"""
