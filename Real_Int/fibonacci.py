fa3d6308586ff75504f2f35aff3b430953bbd7ba
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-16 19:51:25 -0600

    update program

--- Real_Int/fibonacci.py
+++ Real_Int/fibonacci.py
@@ -1,11 +1,12 @@
 # Fibonacci
 
-# The Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones, usually starting with 0 and 1. 
+# The Fibonacci series is a sequence of numbers where each number is the sum of the two preceding ones,
+#  usually starting with 0 and 1. 
 
 
 a, b = 0, 1
 while a <= 10:
-    print(a, end=" ")
+    print(a)
     a, b = b, a + b
 
 # With input
@@ -13,5 +14,5 @@ while a <= 10:
 a, b = 0, 1
 n = int(input("Enter the number for the Fibonacci series: "))
 while a <= n:
-    print(a, end=" ")
+    print(a)
     a, b = b, a + b
