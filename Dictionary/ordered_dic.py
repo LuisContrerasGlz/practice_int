b30cacb73a75340560b70cf41627d361c0bfff09
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-02-21 12:41:26 -0600

    add program

--- Dictionary/ordered_dic.py
+++ Dictionary/ordered_dic.py
@@ -17,3 +17,17 @@ od["e"] = 30
 od["f"] = 80
 
 print(od)
+
+# Other ways to make an OrderedDict
+
+# Passing a List of Tuples:
+
+items = [('a', 10), ('b', 500), ('c', 200), ('d', 2), ('e', 30), ('f', 80)]
+od = OrderedDict(items)
+print(od)
+
+# Using a Dictionary:
+
+my_dict = {'a': 10, 'b': 500, 'c': 200, 'd': 2, 'e': 30, 'f': 80}
+od = OrderedDict(my_dict)
+print(od)
