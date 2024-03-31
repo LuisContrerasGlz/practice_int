2a524e32542b0494986012fef6754f65c7af5ac6
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-03-31 03:29:43 -0600

    progress

--- working_files/pandas_frame_listr.py
+++ working_files/pandas_frame_listr.py
@@ -14,3 +14,23 @@ cocheB = ["K3", 5000, True]
 # Create a DataFrame df using the data of the two cars and specifying the column names
 df = pd.DataFrame([cocheA, cocheB], columns=columns)
 print(df)
+
+# Method 2: Using ZIP
+
+# Define lists containing data for car brands, prices, and availability
+marcas = ["Audi", "Mercedes", "BMW", "Kia"]
+
+precio = [2000,12040, 4000, 5000]
+
+disponibilidad = [True, False, True, True]
+
+# Create a DataFrame df2 using the zip() function to combine data from the lists,
+# and specifying the column names using the columns parameter
+
+df2 = pd.DataFrame(
+    list(zip(marcas, precio, disponibilidad)),
+    columns = ["Marca", "precio", "disponibilidad"]
+
+)
+
+print(df2)
