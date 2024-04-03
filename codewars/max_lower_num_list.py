ac363b23d2045a95a11e1fd242f31f662be4d3e0
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-04-02 21:02:16 -0600

    progress

--- codewars/max_lower_num_list.py
+++ codewars/max_lower_num_list.py
@@ -14,3 +14,30 @@ numbers = [3, 7, 1, 9, 5]
 major, lowest = get_major_and_lowest(numbers)
 print("Major number:", major)
 print("Lowest number:", lowest)
+
+
+# Without build in functions
+
+def get_major_and_lowest_no_build_in(numbers2):
+
+    if not numbers2:
+        return None, None
+    
+    major_num = numbers2[0]
+    lowest_num = numbers2[0]
+
+    for num in numbers2:
+        if num > major_num:
+            major_num = num
+        elif num < lowest_num:
+            lowest_num = num
+
+    return major_num, lowest_num
+
+# Testing:
+numbers2_check = [3, 7, 1, 9, 5]
+major_num, lowest_num = get_major_and_lowest_no_build_in(numbers2_check)
+print("Major number:", major_num)
+print("Lowest number:", lowest_num)
+
+
