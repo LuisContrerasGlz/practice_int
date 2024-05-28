5104c7932e1e2b30fcc72a2d7b5ba93c077edc4c
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-05-27 23:02:57 -0600

    progress

--- String_Problems/Del_test_reverse_string.py
+++ String_Problems/Del_test_reverse_string.py
@@ -36,3 +36,25 @@ print("--------------")
 texto = "Hola este es un ejemplo con todas las palabras"
 texto_inverso = invertir_palabras(texto)
 print(texto_inverso)
+
+# Usando for
+
+def invertir_palabras2(cadena2):
+    # Convertir la cadena en una lista de palabras
+    palabras2 = cadena2.split()
+    # Inicializar una lista vacía para almacenar las palabras en orden inverso
+    palabras_invertidas = []
+    
+    # Usar un bucle for para iterar a través de las palabras
+    for palabra in palabras2:
+        # Insertar cada palabra al inicio de la lista invertida
+        palabras_invertidas.insert(0, palabra)
+    
+    # Unir las palabras invertidas en una cadena
+    cadena_invertida = " ".join(palabras_invertidas)
+    return cadena_invertida
+
+# Ejemplo de uso
+texto_prueba2 = "Hola esto es una prueba"
+texto_revertido2 = invertir_palabras2(texto_prueba2)
+print(texto_revertido2)  # Output: "prueba una es esto Hola"
