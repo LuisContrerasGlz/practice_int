8ec153d05b5130809319c9179156532c88d0cf2d
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-24 00:06:34 -0600

    progress

--- core_problems/remove_leading_zeros.py
+++ core_problems/remove_leading_zeros.py
@@ -1,1 +1,14 @@
 # Remove Leading zeros from a list
+
+def remove_leading_zeros(nums):
+    # Edge case: if the list is empty, return an empty list
+    if not nums:
+        return []
+    
+    # Find the index of the first non-zero element
+    first_non_zero_index = 0
+    while first_non_zero_index < len(nums) and nums[first_non_zero_index] == 0:
+        first_non_zero_index += 1
+    
+    # Return the sublist from the first non-zero element to the end
+    return nums[first_non_zero_index:]
