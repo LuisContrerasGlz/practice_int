b674fe7ba86227153a2b70cf7c08ca5891d9d18e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-24 00:09:00 -0600

    progress

--- core_problems/sort_asc_array.py
+++ core_problems/sort_asc_array.py
@@ -1,1 +1,12 @@
 # Sort the elements of an array in ascending order 
+
+# Using Python's Built-in sorted() Function, creates new one
+
+def sort_array_ascending(arr):
+    sorted_arr = sorted(arr)
+    return sorted_arr
+
+# Using the sort() Method (in-place sorting)
+
+def sort_array_ascending_inplace(arr):
+    arr.sort()
