7b1037c25e531c84ae415661b7afe4986405eef9
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-24 00:05:33 -0600

    progress

--- core_problems/print_sum_items_array.py
+++ core_problems/print_sum_items_array.py
@@ -1,1 +1,10 @@
 # Print the sum of all the items of the array 
+
+def sum_items(items):
+    sum_of_items = 0
+    for i in items:
+        sum_of_items = sum_of_items + i
+    
+    return sum_of_items
+
+print(sum_items([2,2,2,2]))
