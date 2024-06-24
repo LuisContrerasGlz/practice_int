9d6e1665a2b42bb6041ecb4c800a824d89b2e86d
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-23 23:55:41 -0600

    progress

--- core_problems/print_first_letter_string.py
+++ core_problems/print_first_letter_string.py
@@ -1,1 +1,17 @@
-# Print first letter of each word in a string 
+def first_letter(str_check):
+    # Split the input string into words
+    words_str = str_check.split()
+    
+    # Create a list to hold the first letters of each word
+    first_letters = []
+    
+    # Iterate through each word in the split string
+    for word in words_str:
+        # Append the first letter of each word to the list
+        first_letters.append(word[0])
+    
+    # Return the list of first letters
+    return first_letters
+
+# Example usage:
+print(first_letter("Hey This is a test"))  # Output: ['H', 'T', 'i', 'a', 't']
--- core_problems/print_odd_even_numbers.py
+++ core_problems/print_odd_even_numbers.py
@@ -1,1 +1,15 @@
 # In an Array print Odd and Even Numbers 
+
+def odd_even(list_nums):
+    odd_nums = []
+    even_nums = []
+
+    for i in list_nums:
+        if i % 2 == 0:
+            even_nums.append(i)
+        else:
+            odd_nums.append(i)
+    
+    return f"Odd numbers are {odd_nums}, even numbers are {even_nums}"
+
+print(odd_even([2,3,4,5,6,7,8,10]))
