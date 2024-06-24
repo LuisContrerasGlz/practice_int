67ba65578497cfe4edeee00e567fc574b2d4f81e
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-23 18:31:31 -0600

    progress

---
+++ core_problems/first_equals_last.py
@@ -0,0 +1,3 @@
+# Write a function to check if the first element of a list is equal to the last one,
+# if it is print "Same" if not print "Not the same"
+# if the list is empty print "Nothing to check, sorry"
