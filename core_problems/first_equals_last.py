31ea0676c21d61fa774a448d092b11c6c3892c77
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-23 18:45:02 -0600

    progress

--- core_problems/first_equals_last.py
+++ core_problems/first_equals_last.py
@@ -1,3 +1,14 @@
 # Write a function to check if the first element of a list is equal to the last one,
 # if it is print "Same" if not print "Not the same"
 # if the list is empty print "Nothing to check, sorry"
+
+def check_first_last_equality(elements_list):
+
+    if len(elements_list) == 0:
+        print("Nothing to check, sorry")
+    elif elements_list[0] == elements_list[-1]:
+        print("Same")
+    else:
+        print("Not the Same")
+
+check_first_last_equality([1,2,3,4,1])
