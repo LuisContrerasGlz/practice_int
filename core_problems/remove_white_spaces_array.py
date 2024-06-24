75b2682b7e68ac274633c81cda45852cb27522c5
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-23 22:11:12 -0600

    progress

--- core_problems/remove_white_spaces_array.py
+++ core_problems/remove_white_spaces_array.py
@@ -1,1 +1,14 @@
 # From an Array remove all the white spaces
+
+
+def remove_whitespaces(array):
+    new_lst = []
+    for element in array:
+        no_space = element.replace(" ", "")
+        new_lst.append(no_space)
+    
+    return new_lst
+    
+input_array = ["hello world", "  example  ", "remove whitespaces"]
+cleaned_array = remove_whitespaces(input_array)
+print(cleaned_array)
--- core_problems/reverse_string.py
+++ core_problems/reverse_string.py
@@ -1,1 +1,12 @@
 # Reverse a string 
+
+def reverse_str(str_reverse):
+    return str_reverse[::-1]
+
+def reverse_str2(str_reverse2):
+    reversed_str = ""
+
+    for i in str_reverse2:
+        reversed_str = i + reversed_str
+
+    return reversed_str
