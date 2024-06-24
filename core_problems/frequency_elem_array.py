caf2325beacad280f7f40b1e2ccce9ed3445961b
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-06-23 19:00:22 -0600

    progress

--- core_problems/frequency_elem_array.py
+++ core_problems/frequency_elem_array.py
@@ -1,1 +1,20 @@
 # Find the frequency of each element in the array 
+
+def find_frequencies(array):
+    frequency_dict = {}
+    
+    for element in array:
+        # Check if the element is already in the dictionary.
+        if element in frequency_dict:
+            # Increment the count of the element if it is already in the dictionary.
+            frequency_dict[element] += 1
+        # Initializes the count of the element to 1 if it is not in the dictionary.
+        else:
+            frequency_dict[element] = 1
+            
+    # Iterate through the dictionary items and print each element(key) and its corresponding frequency(value).        
+    for element, frequency in frequency_dict.items():
+        print(f"Element: {element}, Frequency: {frequency}")
+
+array = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+find_frequencies(array)
