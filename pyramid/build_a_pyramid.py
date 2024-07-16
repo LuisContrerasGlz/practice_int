f7c84e0dfed0ef8a91c9161dc44570ad75734fa1
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-16 13:25:31 -0600

    pyramid

--- pyramid/build_a_pyramid.py
+++ pyramid/build_a_pyramid.py
@@ -3,3 +3,11 @@
 tamaño = 5
 for i in range(0,tamaño,1):
     print(" " * (tamaño - i - 1) + "*" * (2 * i +1) )
+
+# Funtion
+    
+def pyramid(tamaño_p):
+    for i in range(tamaño_p):
+        print(" " * (tamaño_p - i - 1) + "*" * (2 * i +1) )
+
+pyramid(5)
