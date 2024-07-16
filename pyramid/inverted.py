bb0886305e953ff2df4f73229d288bb21553ee7d
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-16 13:36:16 -0600

    inverted p

--- pyramid/inverted.py
+++ pyramid/inverted.py
@@ -3,3 +3,13 @@
 tamaño = 5
 for i in range(tamaño-1, -1,-1):
     print(" " * (tamaño - i - 1) + "*" * (2 * i +1))
+
+# Funtion
+    
+def inverted_pyramid(tamaño_p):
+    for i in range(tamaño_p-1, -1,-1):
+        print(" " * (tamaño_p - i - 1) + "*" * (2 * i +1) )
+
+inverted_pyramid(5)
+inverted_pyramid(7)
+inverted_pyramid(10)
