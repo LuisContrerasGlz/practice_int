0aa65cc5ab5e95159741981624a54c007ae9a2e9
Luis Francisco Contreras Gonzalez <luis.fco.contreras@gmail.com>
2024-07-17 12:09:18 -0600

    pyramid

--- pyramid/right_inverted_p.py
+++ pyramid/right_inverted_p.py
@@ -2,3 +2,11 @@ tamaño = 5
 
 for i in range(tamaño-1, -1,-1):
     print("*" * (2 * i +1))
+
+def pyramid(tamaño_p):
+    for i in range(tamaño-1, -1,-1):
+        print("*" * (2 * i +1))
+
+pyramid(5)
+pyramid(7)
+pyramid(10)
